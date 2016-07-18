1544be71b12adb3437317e376d43b6a111391f32
cierrajw <cw1572@nyu.edu>
2016-07-18 11:28:55 -0400

    blahblah test

---
+++ ToDoDB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ToDoDB.Startup))]
+namespace ToDoDB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
